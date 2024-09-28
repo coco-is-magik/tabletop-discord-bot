4b458f06e80d2066674c783a87f85ff876f797e1
coco-is-magik <117292842+coco-is-magik@users.noreply.github.com>
2024-09-28 11:11:39 -0400

    Added annotation

--- src/main/java/cocoismagik/commands/SlashCommandListener.java
+++ src/main/java/cocoismagik/commands/SlashCommandListener.java
@@ -1,11 +1,12 @@
 package cocoismagik.commands;
 
+import javax.annotation.Nonnull;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class SlashCommandListener extends ListenerAdapter {
     @Override
-    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+    public void onSlashCommandInteraction(@Nonnull SlashCommandInteractionEvent event) {
         switch(event.getName()) {
             case "ping":
                 Ping.commandLogic(event);

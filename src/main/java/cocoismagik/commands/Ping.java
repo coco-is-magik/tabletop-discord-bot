84225904e0ff4b68f0db2629ff10b4323e2cd031
coco-is-magik <117292842+coco-is-magik@users.noreply.github.com>
2024-09-28 11:11:49 -0400

    Fixed potential null cases

--- src/main/java/cocoismagik/commands/Ping.java
+++ src/main/java/cocoismagik/commands/Ping.java
@@ -1,13 +1,13 @@
 package cocoismagik.commands;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 
 public class Ping{
     public static void commandLogic(SlashCommandInteractionEvent event) {
         event.deferReply().queue(); // Tell discord we received the command, send a thinking... message to the user
-        boolean optionExists = event.getOption("message") != null;
-        String response = (optionExists) ? event.getOption("message").getAsString() : "Pong!";
+        OptionMapping option = event.getOption("message");
+        String response = (option != null) ? option.getAsString() : "Pong!";
         event.getHook().sendMessage(response).queue();
     }
 }

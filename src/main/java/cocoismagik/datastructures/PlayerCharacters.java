4d87957e39b849a8ebe254a4fe36c7e1bbc7f6e2
coco-is-magik <117292842+coco-is-magik@users.noreply.github.com>
2024-11-16 10:41:00 -0500

    Added datastructure for tracking characters and their owners

---
+++ src/main/java/cocoismagik/datastructures/PlayerCharacters.java
@@ -0,0 +1,54 @@
+package cocoismagik.datastructures;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PlayerCharacters {
+    private static PlayerCharacters instance;
+
+    private Map<Long, List<TTRPGChar>> characters;
+
+    private PlayerCharacters() {
+        characters = new HashMap<>();
+    }
+
+    /**
+     * Public static method to get the instance of the class.
+     * 
+     * This method ensures that only one instance of the class is created, and returns that instance.
+     * 
+     * @return the instance of the class
+     */
+    
+    public static PlayerCharacters getInstance() {
+        if (instance == null) {
+            instance = new PlayerCharacters();
+        }
+        return instance;
+    }
+
+    /**
+     * Adds a character to the player's list of characters.
+     * 
+     * If the player does not have a list of characters yet, it will be created.
+     * 
+     * @param playerID the ID of the player to add the character to
+     * @param character the character to add
+     */
+    public void addCharacter(Long playerID, TTRPGChar character) {
+        //TODO: connect to database
+        characters.computeIfAbsent(playerID, k -> new ArrayList<>()).add(character);
+    }
+
+    /**
+     * Retrieves the list of characters associated with a given player ID.
+     * 
+     * @param playerID the ID of the player to retrieve the characters for
+     * @return the list of characters, or null if no characters exist for the given player ID
+     */
+    public List<TTRPGChar> getCharacters(Long playerID) {
+        return characters.get(playerID);
+    }
+}
---
+++ src/main/java/cocoismagik/datastructures/TTRPGChar.java
@@ -0,0 +1,93 @@
+package cocoismagik.datastructures;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cocoismagik.main.DataOutputter;
+
+public class TTRPGChar {
+    // Map for storing abilities (e.g., spells, special abilities, powers)
+    private Map<String, Object> abilities;
+
+    // Map for storing attributes (e.g., strength, dexterity, intelligence)
+    private Map<String, Object> attributes;
+
+    // Map for storing details (e.g., character name, race, class, background info)
+    private Map<String, Object> details;
+
+    // Map for storing statistics (e.g., hit points, level, armor class)
+    private Map<String, Object> statistics;
+
+    private Long playerID;
+    private Long originThreadId;
+
+    public TTRPGChar(Long playerID, Long originThreadId) {
+        this.playerID = playerID;
+        abilities = new HashMap<>();
+        attributes = new HashMap<>();
+        details = new HashMap<>();
+        statistics = new HashMap<>();
+        PlayerCharacters.getInstance().addCharacter(playerID, this);
+    }
+
+    // Methods for interacting with abilities
+    public void addAbility(String abilityName, Object ability) {
+        abilities.put(abilityName, ability);
+    }
+
+    public Object getAbility(String abilityName) {
+        return abilities.get(abilityName);
+    }
+
+    public void removeAbility(String abilityName) {
+        abilities.remove(abilityName);
+    }
+
+    // Methods for interacting with attributes
+    public void addAttribute(String attributeName, Object attribute) {
+        attributes.put(attributeName, attribute);
+    }
+
+    public Object getAttribute(String attributeName) {
+        return attributes.get(attributeName);
+    }
+
+    public void removeAttribute(String attributeName) {
+        attributes.remove(attributeName);
+    }
+
+    // Methods for interacting with details
+    public void addDetail(String detailName, Object detail) {
+        DataOutputter.logMessage(details.toString(), DataOutputter.WARNING);
+        details.put(detailName, detail);
+    }
+
+    public Object getDetail(String detailName) {
+        return details.get(detailName);
+    }
+
+    public void removeDetail(String detailName) {
+        details.remove(detailName);
+    }
+
+    // Methods for interacting with statistics
+    public void addStatistic(String statisticName, Object statistic) {
+        statistics.put(statisticName, statistic);
+    }
+
+    public Object getStatistic(String statisticName) {
+        return statistics.get(statisticName);
+    }
+
+    public void removeStatistic(String statisticName) {
+        statistics.remove(statisticName);
+    }
+
+    public Long getPlayerID() {
+        return playerID;
+    }
+
+    public Long getOriginThreadId() {
+        return originThreadId;
+    }
+}

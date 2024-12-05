99e6e8aee97c53e20e24f779e384305408bf6198
coco-is-magik <117292842+coco-is-magik@users.noreply.github.com>
2024-12-04 22:54:09 -0500

    Simplified the logic a bit

--- src/main/java/cocoismagik/commands/msgprefix/CreateCharacter.java
+++ src/main/java/cocoismagik/commands/msgprefix/CreateCharacter.java
@@ -1,100 +1,92 @@
 package cocoismagik.commands.msgprefix;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import cocoismagik.datastructures.ThreadManagementTracker;
-import cocoismagik.interactables.EmbedWrapper;
 import cocoismagik.main.DataOutputter;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
 import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
 
 public class CreateCharacter {
 
     /**
-     * Retrieves the initial embeds for character creation, including the game selection dropdown.
-     * 
-     * @return a list of EmbedWrapper instances
+     * Sets up a new character creation thread by associating it with a user and
+     * adding the user to the thread. It then sends a game selection embed to the
+     * thread, allowing the user to choose a game for character creation.
+     *
+     * @param thread      the thread channel to set up
+     * @param threadOwner the owner of the thread to associate with the thread
+     * @throws IllegalArgumentException if the thread ownership cannot be established
      */
-    public static List<EmbedWrapper> getCharacterCreationInitialEmbeds() {
-        List<EmbedWrapper> embedList = new ArrayList<>();
+    private static void setupThread(ThreadChannel thread, User threadOwner) {
+        // Need to associate thread with user or the rest of the create character process will fail
+        try{
+            ThreadManagementTracker.addThreadOwnership(threadOwner.getIdLong(), thread.getIdLong());
+        } catch (IllegalArgumentException e) {
+            DataOutputter.logMessage(e.getMessage(), DataOutputter.ERROR);
+            return;
+        }
 
+        // Add the user to the thread
+        thread.addThreadMember(threadOwner).complete();
+
+        //FIXME: this check is broken, it detects failure when the user is already in the thread
+        /* 
+        // Check the user was added to thread
+        if (thread.getThreadMember(threadOwner) != null) {
+            String s = "Made charcreate thread for user " + threadOwner.getName() + " (" + threadOwner.getIdLong() + ")";
+            DataOutputter.logMessage(s, DataOutputter.INFO);
+        } else {
+            String s = "Couldn't make charcreate thread for user " + threadOwner.getName() + " (" + threadOwner.getIdLong() + ")";
+            DataOutputter.logMessage(s, DataOutputter.ERROR);
+        }
+        */
+
+        // Send the game selection embed so the player can choose what kind of character to make
         EmbedBuilder gameSelectionEmbed = new EmbedBuilder();
         gameSelectionEmbed.setTitle("Choose Game");
         gameSelectionEmbed.setDescription("Select a game from the dropdown below.");
 
         ActionRow selectMenu = ActionRow.of(
-                net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu.create("game-selection")
-                        .setPlaceholder("Select game")
-                        .addOption("D&D 5th Edition", "dnd5e")
-                        .build()
+            StringSelectMenu.create("game-selection")
+                .setPlaceholder("Select game")
+                .addOption("D&D 5th Edition", "dnd5e")
+                .build()
         );
 
-        // Create an EmbedWrapper instance and add it to the list
-        embedList.add(new EmbedWrapper(gameSelectionEmbed, List.of(selectMenu)));
-
-        return embedList;
+        MessageCreateAction messageAction = thread.sendMessageEmbeds(gameSelectionEmbed.build());
+        messageAction = messageAction.addActionRow(selectMenu.getComponents());
+        messageAction.queue();
     }
 
+    /**
+     * Handles the "!createcharacter" command issued by a user in a text channel.
+     * 
+     * This method checks if the command is sent from a text channel. If it is, it
+     * creates a private thread for the user to start the character creation process
+     * and sets up the thread with necessary configurations. If the command is not
+     * in a text channel, it sends a reply to the user indicating that the command
+     * is invalid in the current location.
+     * 
+     * @param event The event containing the message and channel information.
+     */
     public static void commandLogic(MessageReceivedEvent event){
+        // Get the message received
         Message message = event.getMessage();
+
+        // Check if the message was sent in a text channel
         if (event.isFromType(ChannelType.TEXT)) {
-            TextChannel channel = event.getChannel().asTextChannel();
-            channel.createThreadChannel("Private Thread", true)
-                    .setAutoArchiveDuration(ThreadChannel.AutoArchiveDuration.TIME_24_HOURS)
-                    .queue(thread -> {
-                        // Associate the thread with the user, and ensure something very strange hasnt happened
-                        try{
-                            ThreadManagementTracker.addThreadOwnership(event.getAuthor().getIdLong(), thread.getIdLong());
-                        } catch (IllegalArgumentException e) {
-                            DataOutputter.logMessage(e.getMessage(), DataOutputter.ERROR);
-                            return;
-                        }
-
-                        // Send an initial message in the thread
-                        thread.sendMessage("This is a private thread for character creation.").queue();
-
-                        List<EmbedWrapper> embedWrappers = getCharacterCreationInitialEmbeds();
-
-                        for (EmbedWrapper wrapper : embedWrappers) {
-                            MessageCreateAction messageAction = thread.sendMessageEmbeds(wrapper.getEmbedBuilder().build());
-
-                            // Add action rows (components) if available
-                            for (ActionRow actionRow : wrapper.getComponents()) {
-                                messageAction = messageAction.addActionRow(actionRow.getComponents());
-                            }
-
-                            // Queue the message to send it
-                            messageAction.queue();
-                        }
-
-                        // Invite the original user who invoked the command
-                        thread.addThreadMember(event.getAuthor()).queue(
-                            success -> {
-                                event.getChannel().sendMessage("You have been added to the private thread!").queue();
-                                String name = event.getAuthor().getName();
-                                Long id = event.getAuthor().getIdLong();
-                                String s = "Made charcreate thread for user named " + name + " with id " + id;
-                                DataOutputter.logMessage(s, DataOutputter.INFO);
-                            },
-                            failure -> {
-                                //FIXME: ping a moderator
-                                event.getChannel().sendMessage("Failed to add you to the private thread.").queue();
-                                String name = event.getAuthor().getName();
-                                Long id = event.getAuthor().getIdLong();
-                                String s = "Failed to make charcreate thread for user named " + name + " with id " + id;
-                                DataOutputter.logMessage(s, DataOutputter.ERROR);
-                            }
-                        );
-                    });
+            // Create the private thread for the user to create their character
+            ThreadChannel thread = event.getChannel().asTextChannel().createThreadChannel("Private Thread", true).complete();
+            setupThread(thread, event.getAuthor());
         } else {
-            message.reply("This command can only be used in a server channel.").queue();
+            message.reply("Invalid location for command, please run the command in a text channel").queue();
+            return;
         }
     }
 }
